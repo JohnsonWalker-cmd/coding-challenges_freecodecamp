714f7a9b89a9343530f680fc2d6908db374c9721
JohnsonWalker-cmd <sakajohnson1@gmail.com>
2026-02-24 23:55:25 +0000

    calculate business day given  start and end dates

---
+++ Day24.js
@@ -0,0 +1,9 @@
+function countBusinessDays(start , end){
+
+    const startDay = parseInt(start.split("-")[2]);
+    const endDay = parseInt(end.split("-")[2]);
+
+    return 
+}
+
+console.log(countBusinessDays('2026-02-24' , '2026-02-26'))
